7f41c34658f5a63130640c42e7def31e2137b300
Niklas Rosenstein <rosensteinniklas@gmail.com>
2019-12-09 14:12:41 +0100

    Add search input in playlist view

--- bbplay-app/src/views/PlaylistView.tsx
+++ bbplay-app/src/views/PlaylistView.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import client, {Playlist} from '../client'
+import {InputGroup, Button} from '@blueprintjs/core'
 
 type PlaylistViewProps = {
   id: number
 }
 
 type PlaylistViewState = {
+  searchString: string
   playlist?: Playlist
 }
 
 export default class PlaylistView extends React.Component<PlaylistViewProps, PlaylistViewState> {
 
-  state: PlaylistViewState = {}
+  state: PlaylistViewState = {searchString: ''}
 
   constructor(props: PlaylistViewProps) {
     super(props)
@@ -21,11 +23,29 @@ export default class PlaylistView extends React.Component<PlaylistViewProps, Pla
     let props = this.props as any
     let playlist = await client.getPlaylist(props.match.params.id)
     this.setState({playlist})
+    this.youtubeSearch = this.youtubeSearch.bind(this)
+  }
+
+  async youtubeSearch(ev: React.MouseEvent<HTMLElement>) {
+    ev.preventDefault()
+    console.log(this.state)
+    alert(this.state.searchString)
   }
 
   render() {
     return <div>
       <h1>{this.state.playlist? this.state.playlist.name : "Loading ..."}</h1>
+      <form><div className="bp3-input-group">
+        <InputGroup
+          value={this.state.searchString}
+          onChange={(ev: React.ChangeEvent<HTMLInputElement>) => {
+            this.setState({searchString: ev.currentTarget.value})
+          }}/>
+        <Button
+          type="submit"
+          icon="arrow-right"
+          onClick={this.youtubeSearch}/>
+      </div></form>
     </div>
   }
 
