d23fc1375d723dec5d53c699296f28b26538a6a8
NiklasRosenstein <NiklasRosenstein@users.noreply.github.com>
2019-12-13 19:33:46 +0000

    Search endpoint now searches for videos only and filters out videos over 10 minutes (closes #22) (#34)

--- bbplay-server/src/bbplay/server/views/api/v1/youtube.py
+++ bbplay-server/src/bbplay/server/views/api/v1/youtube.py
@@ -2,6 +2,30 @@
 from bbplay.framework.rest import resource
 from bbplay.server.app import app, config
 from flask import request
+from nr.parsing.date import parse_iso8601_duration
+import threading
+
+
+class LockedMap(object):
+
+  def __init__(self):
+    self._data = {}
+    self._lock = threading.Lock()
+
+  def __getitem__(self, key):
+    with self._lock:
+      return self._data[key]
+
+  def __setitem__(self, key, value):
+    with self._lock:
+      self._data[key] = value
+
+  def __contains__(self, key):
+    with self._lock:
+      return key in self._data
+
+
+_video_duration_cache = LockedMap()
 
 
 @app.route('/api/v1/youtube/search', methods=['GET'])
@@ -15,8 +39,25 @@ def api_v1_youtube_search():
 
   # TODO (@NiklasRosenstein): Cache the Youtube client instance?
   youtube = config.credentials.create_youtube_client()
-  result = youtube.search(query, pageToken=page_token, maxResults=page_size).page
+  result = youtube.search(
+    query,
+    pageToken=page_token,
+    maxResults=page_size,
+    type='video').page
+
+  def _filter_result(result):
+    video_id = result['id']['videoId']
+    if video_id in _video_duration_cache:
+      duration = _video_duration_cache[video_id]
+    else:
+      result = youtube.get_video(video_id)
+      duration = parse_iso8601_duration(result['contentDetails']['duration'])
+      _video_duration_cache[video_id] = duration
+    if duration > 600:
+      return False
+    return True
+
   return {
     'nextPageToken': result.next_page_token,
-    'values': result.items
+    'values': [x for x in result.items if _filter_result(x)]
   }
